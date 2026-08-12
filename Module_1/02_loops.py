e319564a122476a48f987e99ab615084ec3c4da3
Sanskar Kumar <sanskarkumar838383@gmail.com>
2026-08-13 00:18:50 +0530

    Implement for and while loop examples
    Added examples of for loops and while loops.

---
+++ Module_1/02_loops.py
@@ -0,0 +1,19 @@
+# For Loops
+print("Counting to 5 using a for loop:")
+for number in range(1, 6):
+    print(number)
+
+# Iterating through a list
+colors = ["red", "green", "blue"]
+print("\nIterating through a list:")
+for color in colors:
+    print(f"Color: {color}")
+
+# While Loops
+print("\nCountdown using a while loop:")
+countdown = 3
+while countdown > 0:
+    print(countdown)
+    countdown -= 1
+    
+print("Blastoff!")
